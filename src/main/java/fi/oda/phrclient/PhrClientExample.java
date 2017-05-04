2a351f4058103adeb12cbf23af5bcc0e2baf6a9a
MC\mauno.rönkkö <mauno.ronkko@mediconsult.fi>
2017-05-04 15:19:42 +0300

    Initial commit

---
+++ src/main/java/fi/oda/phrclient/PhrClientExample.java
@@ -0,0 +1,14 @@
+package fi.oda.phrclient;
+
+import org.slf4j.*;
+
+public class PhrClientExample {
+
+    private static Logger logger = LoggerFactory.getLogger(PhrClientExample.class);
+
+    private static final String ODA_PHR_URL = "https://oda.medidemo.fi/phr";
+
+    public static void main(String[] args) throws Exception {
+    }
+
+}
